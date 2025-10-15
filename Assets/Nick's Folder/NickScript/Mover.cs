05a56b81d13d62a50860cb937ba2a3d4110340c5
SNRDEMON <nicholasbstuart@gmail.com>
2025-10-15 18:39:51 -0500

    Just some basic stuffs I guess, not done
    Mainly publishing so I can work on my pc instead of laptop

---
+++ Assets/Nick's Folder/NickScript/Mover.cs
@@ -0,0 +1,24 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+namespace TopDown.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [SerializeField] private float movement_speed;
+        private Rigidbody2D body;
+        private Vector3 currentInput;
+        // Update is called once per frame
+        private void Awake()
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            body.linearVelocity = movement_speed * currentInput * Time.deltaTime;
+        }
+    }
+}

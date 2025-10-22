a3db798dd782fe4944aec27d54fde4276b025aac
samtu391 <158514963+samtu391@users.noreply.github.com>
2025-10-22 09:35:36 -0500

    SMALL WORK

--- Assets/Lane'sAssets/LanesScripts/healthPack.cs
+++ Assets/Lane'sAssets/LanesScripts/healthPack.cs
@@ -5,14 +5,15 @@ public class healthPack : MonoBehaviour
     [SerializeField] private GameObject heathpack;
     private void OnTriggerEnter(Collider other)
     {
-        
 
-        // if (curPlayerhealth < maxplayerhealth)
-        // {
-        //     curPlayerhealth == maxplayerhealh;
-        //
-        // }
-        // else
-        //     return;
+        /*
+        if (curPlayerhealth < maxplayerhealth && other.gameObject.tag == "Player")
+        {
+            curPlayerhealth == maxplayerhealh;
+            Destroy(this);
+        
+         }
+        else
+             return;*/
     }
 }

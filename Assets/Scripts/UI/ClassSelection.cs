349f57a61e3f6ead4ab22a481479bc018b5a5c04
Snow <157506648+Vxvxds@users.noreply.github.com>
2025-11-12 11:22:01 -0600

    added class selection scene

--- Assets/Scripts/UI/ClassSelection.cs
+++ Assets/Scripts/UI/ClassSelection.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class ClassSelection : MonoBehaviour
 {
+    [SerializeField] private GameManager _gM;
     [SerializeField] private Animator _anim;
+    [SerializeField] private AudioSource _aS;
+    [Tooltip("The ID of the class this button selects.")]
+    [SerializeField, Range(0,2)] private uint _classID;
+    private bool _selected;
     private void OnMouseEnter()
     {
         _anim.SetBool("hovered", true);
@@ -11,6 +16,16 @@ public class ClassSelection : MonoBehaviour
 
     private void OnMouseExit()
     {
-        _anim.SetBool("hovered", false);
+        if (!_selected)
+        {
+            _anim.SetBool("hovered", false);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        _selected = !_selected;
+        _gM.SelectClass(_classID);
+        _aS.Play();
     }
 }

3f35f64f23e8dde6aa313e72c8f71a6e7eac23c5
Snow <157506648+Vxvxds@users.noreply.github.com>
2025-11-24 08:31:26 -0600

    forgot to commit...

--- Assets/Scripts/Interactibles/SpikeTrap.cs
+++ Assets/Scripts/Interactibles/SpikeTrap.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Interactibles
 {
@@ -7,12 +8,34 @@ namespace Interactibles
         [Tooltip("The damage that the spike trap deals when activated")]
         [SerializeField] private float _damage;
         [SerializeField] private Animator _anim;
+        private List<PlayerStats> _playersToDamage = new List<PlayerStats>();
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.tag == "Player")
             {
                 _anim.SetBool("active", true);
-                collision.GetComponent<PlayerStats>().TakeDamage(_damage);
+                _playersToDamage.Add(collision.GetComponent<PlayerStats>());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(collision.tag == "Player")
+            {
+                _anim.SetBool("active", false);
+                _playersToDamage.Remove(collision.GetComponent<PlayerStats>());
+            }
+        }
+
+        public void Damage()
+        {
+            if(_playersToDamage.Count <= 0f)
+            {
+                return;
+            }
+            foreach (PlayerStats item in _playersToDamage)
+            {
+                item.TakeDamage(_damage);
             }
         }
     }

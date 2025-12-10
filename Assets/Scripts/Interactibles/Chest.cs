3c8b9c1d5b8e606a8a338c5e4659ed26185f16c9
Snow <157506648+Vxvxds@users.noreply.github.com>
2025-12-10 16:21:26 -0600

    fixed up chest and credits

--- Assets/Scripts/Interactibles/Chest.cs
+++ Assets/Scripts/Interactibles/Chest.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using MapGeneration;
 
 namespace Interactibles
 {
     public class Chest : MonoBehaviour
     {
         private bool _hasActivationed;
+        private bool _canBeOpened;
         [SerializeField] private Sprite _openSprite;
         [SerializeField] private GameObject _canvas;
+        private int _neededKeys;
+        private GameObject _gen;
+
+        public void SetNeededKeys()
+        {
+            _gen = GameObject.FindGameObjectWithTag("Generator");
+            _neededKeys = _gen.GetComponent<Generator>().GetPressurePR;
+            Debug.Log(_neededKeys);
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
@@ -14,13 +25,31 @@ namespace Interactibles
             {
                 return;
             }
-            if (collision.tag == "Player" && collision.GetComponent<PlayerStats>().HasKey)
+            if (collision.tag == "Player" && _canBeOpened)
             {
                 Debug.Log("Get Loot Idiot");
+                Instantiate<GameObject>(_canvas);
                 //_canvas.GetComponent<PopUpText>().PopUp("Get Loot Idiot");
                 this.GetComponent<SpriteRenderer>().sprite = _openSprite;
-                collision.GetComponent<PlayerStats>().Key(false);
+                //collision.GetComponent<PlayerStats>().Key(false);
                 _hasActivationed = true;
+            }
+        }
+        public void AddKey(int key)
+        {
+            /*if(_neededKeys == 0 && !_canBeOpened)
+            {
+                _gen = GameObject.FindGameObjectWithTag("Generator");
+                _neededKeys = _gen.GetComponent<Generator>().GetPressurePR;
+                Debug.Log(_neededKeys);
+            }*/
+
+            _neededKeys += key;
+            Debug.Log(_neededKeys);
+            if (_neededKeys <= 0)
+            {
+                Debug.Log("Can open");
+                _canBeOpened = true;
             }
         }
     }
